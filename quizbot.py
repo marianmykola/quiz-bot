247b3105b7df397b243e35792967a33097ae7bcc
marianmykola <marianmykola88@seznam.cz>
2025-04-21 13:33:15 +0200

    Update quizbot.py

--- quizbot.py
+++ quizbot.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
